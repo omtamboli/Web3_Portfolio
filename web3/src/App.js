c61f3ca294638a1e322a4a49902e7e6da2031121
Om <omtamboli08@gmail.com>
2022-02-16 19:42:02 +0530

    components

--- web3/src/App.js
+++ web3/src/App.js
@@ -2,46 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Nav,Navbar ,Container,NavDropdown} from 'react-bootstrap';
-import { GiWallet } from "react-icons/gi";
 
 
+import Header from "./My-components/Header";
+import Wallet from "./My-components/Wallet";
 function App() {
   return (
-    
-
-    
-
+      
 <>
   
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">Web 3.0</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
+     <Header/>
+     <Wallet/>
 
-        <i class="fa-solid fa-wallet"></i>
-        < GiWallet className='GiWallet-icon'/>
-      <h1 id="please-wallet">please connect wallet</h1>
+        
 
 </>
     
---
+++ web3/src/My-components/Wallet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GiWallet } from "react-icons/gi";
+
+export default function Wallet() {
+  return (
+    <>
+    <i class="fa-solid fa-wallet"></i>
+        < GiWallet className='GiWallet-icon'/>
+      <h1 id="please-wallet">please connect wallet</h1>
+      </>
+  )
+}
